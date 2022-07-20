e5065b24d0563570994d33def9a29262423a35b4
Basil Meyer <basilmeyer1985@gmail.com>
2022-07-20 09:37:15 +0200

    first commit

---
+++ init.js
@@ -0,0 +1,8 @@
+//code for javascript form validation
+
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+
+
+
